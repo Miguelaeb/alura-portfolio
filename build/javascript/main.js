2027ca44277f45c215f88497d512c2ed7ed47eff
miguel evangelista <miguelbranagan30@gmail.com>
2023-06-16 11:38:22 -0400

    form

--- build/javascript/main.js
+++ build/javascript/main.js
@@ -1,12 +1,13 @@
-var iconHumberger = document.querySelector(".icon--humburger");
-var navbarLinksContainer = document.querySelector(".navbar--links--container");
-var logo = document.querySelector(".logo");
-var headerContainer = document.querySelector(".header--container");
-var mainContainer = document.querySelector(".main--container");
-var downloadButton = document.querySelector(".download--button");
-var footerContainer = document.querySelector(".footer--container");
+const iconHumberger = document.querySelector(".icon--humburger");
+const navbarLinksContainer = document.querySelector(".navbar--links--container");
+const logo = document.querySelector(".logo");
+const headerContainer = document.querySelector(".header--container");
+const mainContainer = document.querySelector(".main--container");
+const downloadButton = document.querySelector(".download--button");
+const footerContainer = document.querySelector(".footer--container");
+const form = document.querySelector("#contactForm");
 
-// Close navbar phone menu by clicking outside it 
+// Close navbar phone menu by clicking outside it
 function handleClickOutside(event) {
     if (
         !navbarLinksContainer.contains(event.target) &&
@@ -36,20 +37,38 @@ document.addEventListener("click", handleClickOutside);
 downloadButton.addEventListener("click", function () {
     var link = document.createElement("a");
     link.download = "resume.pdf";
-    link.href = "/build/PDF/resume.pdf"; 
+    link.href = "/build/PDF/resume.pdf";
     link.click();
 });
 
+// Form validation
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    //input files
+    const nameForm = document.querySelector("#name");
+    const emailForm = document.getElementById("email");
+    const subjectForm = document.getElementById("subject");
+    const messageForm = document.getElementById("message");
+
+    // Check if the name input is empty
+    if (nameForm.value.trim() === "") {
+      nameForm.classList.add("border-portfolio-primery-color-red");
+    } else {
+      nameForm.classList.remove("border-portfolio-primery-color-red");
+    }
+});
+
 // Smooth scroll
-window.addEventListener('DOMContentLoaded', () => {
-  const links = document.querySelectorAll('a[href^="#"]');
-  links.forEach(link => {
-    link.addEventListener('click', (e) => {
-      e.preventDefault();
-      const target = document.querySelector(link.getAttribute('href'));
-      target.scrollIntoView({
-        behavior: 'smooth'
-      });
+window.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll('a[href^="#"]');
+    links.forEach((link) => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+            const target = document.querySelector(link.getAttribute("href"));
+            target.scrollIntoView({
+                behavior: "smooth",
+            });
+        });
     });
-  });
 });
